3e25aa7e0ecb23d008c976aa109e783ea83689bf
MannyFortune27 <kvaravind2002@protonmail.com>
2021-10-07 23:29:51 +0530

    edited Search file

--- searchpy.py
+++ searchpy.py
@@ -1,17 +1,31 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 PATH="/home/aravind/Featurepreneur/chromedriver"
 driver = webdriver.Chrome(PATH)
 
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+driver.get("http://techwithtim.net")
+
+print(driver.title)
+
+search=driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+# try:
+#     main=WebDriverWait(driver,10).until(
+#         EC.presence_of_elements_located((By.ID,"main"))
+#     )
+    
+# except:
+#     driver.quit()
+
+
+# print(main.txt)
 
 time.sleep(10)
-driver.close()
+driver.quit()
